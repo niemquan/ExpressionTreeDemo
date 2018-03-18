811cd9830bc3a88654d2f8731c9f51de6336a916
Jason Nie <niemquan@126.com>
2018-03-18 10:26:15 +0800

    create expression tree demo solution

---
+++ ExpressionTreeDemo/ExpresionLambdaDemo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTreeDemo
+{
+    public static class ExpresionLambdaDemo
+    {
+        public static void NumLessFiveDemo()
+        {
+            ParameterExpression num1 = Expression.Parameter(typeof(int), "num1");
+
+            ConstantExpression fiveConstantVariable = Expression.Constant(5);
+
+            BinaryExpression binary = Expression.LessThan(num1, fiveConstantVariable);
+
+            var lambdaExp = Expression.Lambda<Func<int, bool>>(binary, num1);
+
+            var result = lambdaExp.Compile()(10);
+
+            Console.WriteLine("Num Less Five Demo, the parameter is 10 "+result);
+
+        }
+
+        public static void NumLessFiveDemoInitParameter()
+        {
+            ParameterExpression num1 = Expression.Parameter(typeof(int), "num1");
+
+            ConstantExpression fiveConstantVariable = Expression.Constant(5);
+
+            BinaryExpression numLessThenFive = Expression.LessThan(num1, fiveConstantVariable);
+
+            Expression<Func<int, bool>> lambdaExp = Expression.Lambda<Func<int, bool>>(
+                numLessThenFive,
+                new ParameterExpression[] {num1});
+
+            var result = lambdaExp.Compile()(3);
+
+            Console.WriteLine(result);
+        }
+
+        public static void LoopExpressionDemo()
+        {
+            ParameterExpression value = Expression.Parameter(typeof(int), "value");
+            ParameterExpression result = Expression.Parameter(typeof(int), "result");
+
+            LabelTarget label = Expression.Label(typeof(int));
+
+            BlockExpression block = Expression.Block(
+                new[] {result},
+                Expression.Assign(result, Expression.Constant(1)),
+                Expression.Loop(Expression.Loop(Expression.IfThenElse(
+                        Expression.GreaterThan(value, Expression.Constant(1)),
+                        Expression.MultiplyAssign(result, Expression.PostDecrementAssign(value)),
+                        Expression.Break(label, result)
+                    ),
+                    label)
+                )
+            );
+            int factorial = Expression.Lambda<Func<int, int>>(block, value).Compile()(5);
+
+            Console.WriteLine(factorial);
+        }
+
+        public static void ParsingExpressionTree()
+        {
+            Expression<Func<int, bool>> exprTree = num => num < 5;
+
+            ParameterExpression parm = exprTree.Parameters[0];
+            BinaryExpression operation = (BinaryExpression) exprTree.Body;
+            ParameterExpression left = (ParameterExpression) operation.Left;
+            ConstantExpression right = (ConstantExpression) operation.Right;
+
+            Console.WriteLine("Decomposed expression: {0} => {1} {2} {3}",
+                parm.Name,left.Name,operation.NodeType,right.Value);
+
+
+
+        }
+    }
+}
---
+++ ExpressionTreeDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTreeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpresionLambdaDemo.ParsingExpressionTree();
+            ExpresionLambdaDemo.NumLessFiveDemo();
+            ExpresionLambdaDemo.NumLessFiveDemoInitParameter();
+            Console.ReadKey();
+        }
+    }
+}
